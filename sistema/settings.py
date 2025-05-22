b4a1774c6c1201e8c1a2cee86b278f1581e86a20
Dafesigo04 <160060204+Dafesigo04@users.noreply.github.com>
2025-05-21 19:01:53 -0500

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-MEDIA_ROOT =os.path.join(BASE_DIR, '')#otro cambio
+MEDIA_ROOT =os.path.join(BASE_DIR, 'static')#otro cambio
 MEDIA_URL = '/media/'#otro cambios
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
